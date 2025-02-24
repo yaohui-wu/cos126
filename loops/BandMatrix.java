ec41e0f9b795fc714a9ada69aa8019a181661251
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-24 09:32:38 -0500

    Update Band matrices

---
+++ loops/BandMatrix.java
@@ -0,0 +1,26 @@
+public class BandMatrix {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int width = Integer.parseInt(args[1]);
+        for (int row = 0; row < n; row += 1) {
+            for (int col = 0; col < n; col += 1) {
+                int dist = Math.abs(col - row);
+                if (dist > width) {
+                    System.out.print(0);
+                } else {
+                    System.out.print("*");
+                }
+                if (col < n - 1) {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+            if (row < n - 1) {
+                for (int i = 0; i < n; i += 1) {
+                    System.out.print("  ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
