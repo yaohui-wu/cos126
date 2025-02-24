e7056110c451bf50542e4aa771bc02344cac42d4
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-23 20:58:56 -0500

    Update Hello, World

--- hello/GreatCircle.java
+++ hello/GreatCircle.java
@@ -1,4 +1,8 @@
 public class GreatCircle {
+    /**
+     * Computes the great-circle distance (in kilometers) between two points
+     * on the surface of the earth.
+     */
     public static void main(String[] args) {
         double x1 = Double.parseDouble(args[0]);
         double y1 = Double.parseDouble(args[1]);
@@ -8,30 +12,13 @@ public class GreatCircle {
         y1 = Math.toRadians(y1);
         x2 = Math.toRadians(x2);
         y2 = Math.toRadians(y2);
-        double greatCircDist = greatCircDist(x1, y1, x2, y2);
-        System.out.println(greatCircDist + " kilometers");
-    }
-
-    /**
-     * Computes the great-circle distance (in kilometers) between two points
-     * on the surface of the earth.
-     */
-    private static double greatCircDist(
-        double x1,
-        double y1,
-        double x2,
-        double y2
-    ) {
         // Haversine formula.
-        double haversineX = haversine(x1, x2);
-        double haversineY = haversine(y1, y2);
+        double haversineX = Math.pow(Math.sin((x2 - x1) / 2), 2);
+        double haversineY = Math.pow(Math.sin((y2 - y1) / 2), 2);
         haversineY *= Math.cos(x1) * Math.cos(x2);
-        final double R = 6_371; // Mean radius of the Earth (in kilometers).
+        // Mean radius of the Earth (in kilometers).
+        final double R = 6_371.0;
         double dist = 2 * R * Math.asin(Math.sqrt(haversineX + haversineY));
-        return dist;
-    }
-
-    private static double haversine(double a, double b) {
-        return Math.pow(Math.sin((b - a) / 2), 2);
+        System.out.println(dist + " kilometers");
     }
 }
