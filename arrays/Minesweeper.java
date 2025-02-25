edd567dbfe78f1e15daec6ddcedc961a64d87389
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-25 17:33:26 -0500

    Update Minesweeper

--- arrays/Minesweeper.java
+++ arrays/Minesweeper.java
@@ -1,38 +1,57 @@
 public class Minesweeper {
     public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+        int k = Integer.parseInt(args[2]);
         /*
          * Generate an M-by-N grid of cells, with exactly K of the M * N cells
          * containing mines, uniformly at random.
          */
-        int m = Integer.parseInt(args[0]);
-        int n = Integer.parseInt(args[1]);
-        int k = Integer.parseInt(args[2]);
-        boolean[][] mines = new boolean[m + 2][n + 2];
-        int[][] neighborMines = new int[m][n];
+        int[][] cells = new int[m + 2][n + 2];
         for (int i = 0; i < k; i += 1) {
             int row = (int) (Math.random() * m) + 1;
             int col = (int) (Math.random() * n) + 1;
-            if (!mines[row][col]) {
-                mines[row][col] = true;
+            if (cells[row][col] != -1) {
+                cells[row][col] = -1; // Put a mine.
             }
         }
         for (int row = 1; row <= m; row += 1) {
             for (int col = 1; col <= n; col += 1) {
-                for (int i = row - 1; i < row + 2; i += 1) {
-                    for (int j = col - 1; j < col + 2; j += 1) {
-                        if (i != j && mines[i][j]) {
-                            neighborMines[row - 1][col - 1] += 1;
-                        }
+                if (cells[row][col] == -1) {
+                    if (cells[row - 1][col - 1] != -1) {
+                        cells[row - 1][col - 1] += 1;
+                    }
+                    if (cells[row - 1][col] != -1) {
+                        cells[row - 1][col] += 1;
+                    }
+                    if (cells[row - 1][col + 1] != -1) {
+                        cells[row - 1][col + 1] += 1;
+                    }
+                    if (cells[row - 1][col - 1] != -1) {
+                        cells[row - 1][col - 1] += 1;
+                    }
+                    if (cells[row - 1][col + 1] != -1) {
+                        cells[row - 1][col + 1] += 1;
+                    }
+                    if (cells[row + 1][col - 1] != -1) {
+                        cells[row + 1][col - 1] += 1;
+                    }
+                    if (cells[row + 1][col] != -1) {
+                        cells[row + 1][col] += 1;
+                    }
+                    if (cells[row + 1][col + 1] != -1) {
+                        cells[row + 1][col + 1] += 1;
                     }
                 }
             }
         }
         for (int row = 1; row <= m; row += 1) {
             for (int col = 1; col <= n; col += 1) {
-                if (mines[row][col]) {
+                int cell = cells[row][col];
+                if (cell == -1) {
                     System.out.print("*");
                 } else {
-                    System.out.print(neighborMines[row - 1][col - 1]);
+                    System.out.print(cell);
                 }
                 if (col < n) {
                     System.out.print("  ");
