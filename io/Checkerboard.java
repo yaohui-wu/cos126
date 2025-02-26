c9769f5ad124c6b21fd5a1d33033ba839d6dc418
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-26 11:50:14 -0500

    Update Checkerboard

--- io/Checkerboard.java
+++ io/Checkerboard.java
@@ -5,7 +5,7 @@ public class Checkerboard {
         double halfLength = 0.5;
         for (int row = 0; row < n; row += 1) {
             for (int col = 0; col < n; col += 1) {
-                if (row % 2 == col % 2) {
+                if ((row + col) % 2 == 0) {
                     StdDraw.setPenColor(StdDraw.BLUE);
                 } else {
                     StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
