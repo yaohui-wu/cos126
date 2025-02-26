7f0376b329de835b8a992fdf0a3d807fc471850e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-26 11:38:42 -0500

    Update World maps

---
+++ io/WorldMap.java
@@ -0,0 +1,20 @@
+public class WorldMap {
+    public static void main(String[] args) {
+        int width = StdIn.readInt();
+        int height = StdIn.readInt();
+        StdDraw.setCanvasSize(width, height);
+        StdDraw.setXscale(0, width);
+        StdDraw.setYscale(0, height);
+        while (!StdIn.isEmpty()) {
+            StdIn.readString(); // Skip the region name.
+            int num = StdIn.readInt(); // Number of vertices.
+            double[] xs = new double[num];
+            double[] ys = new double[num];
+            for (int i = 0; i < num; i += 1) {
+                xs[i] = StdIn.readDouble();
+                ys[i] = StdIn.readDouble();
+            }
+            StdDraw.polygon(xs, ys);
+        }
+    }
+}
