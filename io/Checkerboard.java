8a36fca5645b867b01f292c3393588057fe51cad
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-26 11:19:21 -0500

    Update Checkerboard

---
+++ io/Checkerboard.java
@@ -0,0 +1,19 @@
+public class Checkerboard {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        StdDraw.setScale(0, n);
+        double halfLength = 0.5;
+        for (int row = 0; row < n; row += 1) {
+            for (int col = 0; col < n; col += 1) {
+                if (row % 2 == col % 2) {
+                    StdDraw.setPenColor(StdDraw.BLUE);
+                } else {
+                    StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+                }
+                double x = row + halfLength;
+                double y = col + halfLength;
+                StdDraw.filledSquare(x, y, halfLength);
+            }
+        }
+    }
+}
