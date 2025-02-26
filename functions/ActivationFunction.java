615a7fff52e33f6b375fe099c5f4612f2c20d6d9
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-26 14:23:43 -0500

    Update Activation functions

---
+++ functions/ActivationFunction.java
@@ -0,0 +1,21 @@
+public class ActivationFunction {
+
+    // Returns the Heaviside function of x.
+    public static double heaviside(double x)
+
+    // Returns the sigmoid function of x.
+    public static double sigmoid(double x)
+
+    // Returns the hyperbolic tangent of x.
+    public static double tanh(double x)
+
+    // Returns the softsign function of x.
+    public static double softsign(double x)
+
+    // Returns the square nonlinearity function of x.
+    public static double sqnl(double x)
+
+    // Takes a double command-line argument x and prints each activation
+    // function, evaluated, in the format (and order) given below.
+    public static void main(String[] args)
+}
