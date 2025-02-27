1f403b29bad3d42099eac86568bde51e7caabc86
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-27 18:12:53 -0500

    Update Trinomial coefficients (brute force)

--- recursion/TrinomialBrute.java
+++ recursion/TrinomialBrute.java
@@ -1,8 +1,30 @@
 public class TrinomialBrute {
+    /**
+     * Returns the trinomial coefficient T(N, K).
+     */
+    public static long trinomial(int n, int k) {
+        // Base case.
+        long tri = 1;
+        if (n == 0 && k == 0) {
+            return tri;
+        }
+        if (k < -n || k > n) {
+            tri = 0;
+            return tri;
+        }
+        // Recurrence relation.
+        tri = trinomial(n - 1, k - 1) + trinomial(n - 1, k);
+        tri += trinomial(n - 1, k + 1);
+        return tri;
+    }
 
-    // Returns the trinomial coefficient T(n, k).
-    public static long trinomial(int n, int k)
-
-    // Takes two integer command-line arguments n and k and prints T(n, k).
-    public static void main(String[] args)
+    /**
+     * Takes two integer command-line arguments N and K and prints T(N, K).
+     */
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[1]);
+        long tri = trinomial(n, k);
+        System.out.println(tri);
+    }
 }
