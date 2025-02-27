89c5b0e1c0a22b54c3c96f229912c8ca2900ad23
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-26 21:05:38 -0500

    Update Greatest common divisors

---
+++ functions/Divisors.java
@@ -0,0 +1,19 @@
+public class Divisors {
+
+    // Returns the greatest common divisor of a and b.
+    public static int gcd(int a, int b)
+
+    // Returns the least common multiple of a and b.
+    public static int lcm(int a, int b)
+
+    // Returns true if a and b are relatively prime; false otherwise.
+    public static boolean areRelativelyPrime(int a, int b)
+
+    // Returns the number of integers between 1 and n that are
+    // relatively prime with n.
+    public static int totient(int n)
+
+    // Takes two integer command-line arguments a and b and prints
+    // each function, evaluated in the format (and order) given below.
+    public static void main(String[] args)
+}
