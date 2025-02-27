d38def06d94ef3df6ba5c28af6b78f995b4e744d
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-27 12:09:04 -0500

    Update Greatest common divisors

--- functions/Divisors.java
+++ functions/Divisors.java
@@ -1,19 +1,77 @@
 public class Divisors {
+    /**
+     * Returns the greatest common divisor of A and B.
+     */
+    public static int gcd(int a, int b) {
+        // Euclidean algorithm.
+        int gcd = 0;
+        if (a == 0 && b == 0) {
+            return gcd;
+        }
+        if (a == 1 || b == 1) {
+            gcd = 1;
+            return gcd;
+        }
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (b != 0) {
+            // Replace (A, B) with (B, A mod B).
+            int temp = a;
+            a = b;
+            b = temp % b;
+        }
+        return a;
+    }
 
-    // Returns the greatest common divisor of a and b.
-    public static int gcd(int a, int b)
+    /**
+     * Returns the least common multiple of A and B.
+     */
+    public static int lcm(int a, int b) {
+        int lcm = 0;
+        if (a == 0 && b == 0) {
+            return lcm;
+        }
+        // Perform division first to avoid overflow.
+        lcm = Math.abs(a) / gcd(a, b) * Math.abs(b);
+        return lcm;
+    }
 
-    // Returns the least common multiple of a and b.
-    public static int lcm(int a, int b)
+    /**
+     * Returns true if A and B are relatively prime; false otherwise.
+     */
+    public static boolean areRelativelyPrime(int a, int b) {
+        return gcd(a, b) == 1;
+    }
 
-    // Returns true if a and b are relatively prime; false otherwise.
-    public static boolean areRelativelyPrime(int a, int b)
+    /**
+     * Returns the number of integers between 1 and N that are relatively
+     * prime with N.
+     */
+    public static int totient(int n) {
+        // Euler's totient function phi(n).
+        int phi = 0;
+        if (n <= 0) {
+            return phi;
+        }
+        for (int i = 1; i < n; i += 1) {
+            if (areRelativelyPrime(i, n)) {
+                phi += 1;
+            }
+        }
+        return phi;
+    }
 
-    // Returns the number of integers between 1 and n that are
-    // relatively prime with n.
-    public static int totient(int n)
-
-    // Takes two integer command-line arguments a and b and prints
-    // each function, evaluated in the format (and order) given below.
-    public static void main(String[] args)
+    /**
+     * Takes two integer command-line arguments A and B and prints each
+     * function.
+     */
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.printf("gcd(%d,%d) = %d", a, b, gcd(a,b));
+        System.out.printf("lcm(%d,%d) = %d", a, b, lcm(a,b));
+        System.out.printf("areRelativelyPrime(%d,%d) = %d", a, b, areRelativelyPrime(a,b));
+        System.out.printf("totient(%d) = %d", a, totient(a));
+        System.out.printf("totient(%d) = %d", b, totient(b));
+    }
 }
