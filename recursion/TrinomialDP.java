83ba6a63a9b33cdb4ad3ca2e49e0f0e154f9d525
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-28 14:11:09 -0500

    Update Trinomial coefficients (dynamic programming)

--- recursion/TrinomialDP.java
+++ recursion/TrinomialDP.java
@@ -4,24 +4,43 @@ public class TrinomialDP {
      */
     public static long trinomial(int n, int k) {
         long tri = 0;
-        if (n == 0 && k == 0) {
+        // T(N, K) = T(N, -K).
+        k = Math.abs(k);
+        if (n < k) {
+            return tri;
+        }
+        // T(N, N) = 1.
+        if (n == k) {
             tri = 1;
             return tri;
         }
-        k = Math.abs(k); // Fact: T(N, K) = T(N, -K).
-        if (k > n) {
-            return tri;
-        }
         // Dynamic programming (bottom-up).
-        long[][] tris = new long[n + 1][k + 2];
+        int size = n + 1;
+        long[][] tris = new long[size][size];
         tris[0][0] = 1;
-        for (int i = 1; i < k + 2; i += 1) {
-            tris[0][i] = 0;
-        }
-        for (int i = 1; i < n + 1; i += 1) {
-            for (int j = 1; j < k + 1; j += 1) {
-                tris[i][j] = 0;
+        for (int i = 1; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                long t = 0;
+                if (i > j) {
+                    int m = i - 1;
+                    if (j - 1 == -1) {
+                        t = tris[m][1];
+                    } else {
+                        t = tris[m][j - 1];
+                    }
+                    t += tris[m][j] + tris[m][j + 1];
+                } else if (i == j) {
+                    t = 1;
+                }
+                tris[i][j] = t;
             }
+        }
+        // TODO: Show output for debugging (remove this after algorithm works).
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                System.out.print(tris[i][j] + " ");
+            }
+            System.out.println();
         }
         tri = tris[n][k];
         return tri;
@@ -34,6 +53,7 @@ public class TrinomialDP {
         int n = Integer.parseInt(args[0]);
         int k = Integer.parseInt(args[1]);
         long tri = trinomial(n, k);
-        System.out.println(tri);
+        // TODO: Show output for debugging (change this after algorithm works).
+        System.out.printf("T(%d, %d) = %d\n", n, k, tri);
     }
 }
