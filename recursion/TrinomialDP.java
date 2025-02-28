585fc0861612f6c8311c2cc7bf79f377ca6de879
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-27 19:26:54 -0500

    Update Trinomial coefficients (dynamic programming)

--- recursion/TrinomialDP.java
+++ recursion/TrinomialDP.java
@@ -1,8 +1,26 @@
 public class TrinomialDP {
+    /**
+     * Returns the trinomial coefficient T(N, K).
+     */
+    public static long trinomial(int n, int k) {
+        long tri = 1;
+        if (n == 0 && k == 0) {
+            return tri;
+        }
+        if (k < -n || k > n) {
+            tri = 0;
+            return tri;
+        }
+        return tri;
+    }
 
-    // Returns the trinomial coefficient T(n, k).
-    public static long trinomial(int n, int k)
-
-    // Takes two integer command-line arguments n and k and prints T(n, k).
-    public static void main(String[] args)
+    /**
+     * Takes two integer command-line arguments N and K and prints T(N, K).
+     */
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[1]);
+        long tri = trinomial(n, k);
+        System.out.println(tri);
+    }
 }
