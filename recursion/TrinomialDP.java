41c62791f59101aa64ea58337fe20a67f9d170f2
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-01 14:39:23 -0500

    Update Trinomial coefficients (dynamic programming)

--- recursion/TrinomialDP.java
+++ recursion/TrinomialDP.java
@@ -17,30 +17,36 @@ public class TrinomialDP {
         int size = n + 1;
         long[][] tris = new long[size][]; // Trinomial triangle.
         for (int i = 0; i < size; i += 1) {
-            tris[i] = new long[size - i + k];
+            tris[i] = new long[i + 1];
         }
         // Dynamic programming (bottom-up).
         tris[0][0] = 1;
+        // T(N, K) = T(N - 1, K - 1) + T(N - 1, K) + T(N - 1, K + 1).
         for (int i = 1; i < size; i += 1) {
-            for (int j = 0; j < size - i + k; j += 1) {
-                long t = 0;
-                if (i > j) {
+            for (int j = 0; j < tris[i].length; j += 1) {
+                long t = 1;
+                if (i != j) {
+                    t = 0;
                     int m = i - 1;
-                    if (j - 1 == -1) {
-                        t = tris[m][1];
-                    } else {
-                        t = tris[m][j - 1];
+                    int len = tris[m].length;
+                    if (j - 1 < 0 && j + 1 < len) {
+                        t += tris[m][j + 1];
+                    } else if (j - 1 >= 0) {
+                        t += tris[m][j - 1]; // T(N - 1, K - 1).
                     }
-                    t += tris[m][j] + tris[m][j + 1];
-                } else if (i == j) {
-                    t = 1;
+                    if (j < len) {
+                        t += tris[m][j]; // T(N - 1, K).
+                    }
+                    if (j + 1 < len) {
+                        t += tris[m][j + 1]; // T(N - 1, K + 1).
+                    }
                 }
                 tris[i][j] = t;
             }
         }
         // TODO: Show output for debugging (remove this after algorithm works).
         for (int i = 0; i < size; i += 1) {
-            for (int j = 0; j < size - i + k; j += 1) {
+            for (int j = 0; j < tris[i].length; j += 1) {
                 System.out.print(tris[i][j] + " ");
             }
             System.out.println();
