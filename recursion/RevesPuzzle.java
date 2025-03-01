42a5ae5daea94e6ec5e70540c9485ac90efb7b91
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-28 22:25:08 -0500

    Update Reve's puzzle

--- recursion/RevesPuzzle.java
+++ recursion/RevesPuzzle.java
@@ -1,8 +1,28 @@
 public class RevesPuzzle {
-    private static void RevesPuzzle(int n) {
-        int k = (int) Math.round(n + 1 - Math.sqrt(2 * n + 1));
-    }
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
+        reves(n, "A", "B", "C", "D");
+    }
+
+    private static void reves(
+        int n,
+        String from,
+        String temp1,
+        String temp2,
+        String to
+    ) {
+        if (n == 0) {
+            return;
+        }
+        StdOut.printf("Move disc %d from %s to %s\n", n, from, to);
+        // Frame–Stewart algorithm.
+        int k = (int) Math.round(n + 1 - Math.sqrt(2 * n + 1));
+    }
+
+    private static void hanoi(int n, String from, String temp, String to) {
+        if (n == 0) {
+            return;
+        }
+        StdOut.printf("Move disc %d from %s to %s\n", n, from, to);
     }
 }
