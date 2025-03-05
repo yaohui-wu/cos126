8a90caf639d0356d333a3ac546f4c510b118a3d0
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-05 17:15:26 -0500

    Update inversions

--- performance/Inversions.java
+++ performance/Inversions.java
@@ -2,18 +2,37 @@ public class Inversions {
     /**
      * Return the number of inversions in the permutation a[].
      */
-    public static long count(int[] a) {}
+    public static long count(int[] a) {
+        long count = 0;
+        int length = a.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i < j && a[i] > a[j]) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
 
     /**
      * Return a permutation of length n with exactly k inversions.
      */
-    public static int[] generate(int n, long k) {}
+    public static int[] generate(int n, long k) {
+        int[] p = new int[n]; // Permutation.
+        return p;
+    }
 
-    // Takes an integer n and a long k as command-line arguments,
-    // and prints a permutation of length n with exactly k inversions.
     /**
      * Takes an integer n and a long k as command-line arguments, and prints a
      * permutation of length n with exactly k inversions.
      */
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        long k = Long.parseLong(args[1]);
+        int[] p = generate(n, k);
+        for (int i = 0; i < n; i++) {
+            StdOut.print(p[i] + " ");
+        }
+    }
 }
