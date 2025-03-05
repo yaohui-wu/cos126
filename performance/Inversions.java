57b592aac73976eb6ddec2f098c42711f77f55ca
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-04 21:20:28 -0500

    Update inversions

---
+++ performance/Inversions.java
@@ -0,0 +1,19 @@
+public class Inversions {
+    /**
+     * Return the number of inversions in the permutation a[].
+     */
+    public static long count(int[] a) {}
+
+    /**
+     * Return a permutation of length n with exactly k inversions.
+     */
+    public static int[] generate(int n, long k) {}
+
+    // Takes an integer n and a long k as command-line arguments,
+    // and prints a permutation of length n with exactly k inversions.
+    /**
+     * Takes an integer n and a long k as command-line arguments, and prints a
+     * permutation of length n with exactly k inversions.
+     */
+    public static void main(String[] args) {}
+}
