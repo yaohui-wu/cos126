1924455cc4b993cbc08ad0dacdcf7f939191a19f
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-04 21:21:17 -0500

    Update maximum square submatrix

---
+++ performance/MaximumSquareSubmatrix.java
@@ -0,0 +1,15 @@
+public class MaximumSquareSubmatrix {
+    // Returns the size of the largest contiguous square submatrix
+    // of a[][] containing only 1s.
+    /**
+     * Returns the size of the largest contiguous square submatrix of a[][]
+     * containing only 1s.
+     */
+    public static int size(int[][] a) {}
+
+    /**
+     * Reads an n-by-n matrix of 0s and 1s from standard input and prints the
+     * size of the largest contiguous square submatrix containing only 1s.
+     */
+    public static void main(String[] args) {}
+}
