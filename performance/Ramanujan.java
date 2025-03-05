79a643878da1a755b06217c111f38c065415b935
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-04 21:20:57 -0500

    Update Ramanujan numbers

---
+++ performance/Ramanujan.java
@@ -0,0 +1,12 @@
+public class Ramanujan {
+    /**
+     * Is n a Ramanujan number?
+     */
+    public static boolean isRamanujan(long n) {}
+
+    /**
+     * Takes a long integer command-line arguments n and prints true if n is a
+     * Ramanujan number, and false otherwise.
+     */
+    public static void main(String[] args) {}
+}
