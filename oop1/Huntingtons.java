3086bc5f6142b8bc2e390a84616cda73cab5340e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-07 19:05:34 -0500

    Update Huntington's disease detector

---
+++ oop1/Huntingtons.java
@@ -0,0 +1,22 @@
+public class Huntingtons {
+    /**
+     * Returns the maximum number of consecutive repeats of CAG in the DNA string.
+     */
+    public static int maxRepeats(String dna) {}
+
+    /**
+     * Returns a copy of s, with all whitespace (spaces, tabs, and newlines) removed.
+     */
+    public static String removeWhitespace(String s) {}
+
+    /**
+     * Returns one of these diagnoses corresponding to the maximum number of
+     * repeats: "not human", "normal", "high risk", or "Huntington's".
+     */
+    public static String diagnose(int maxRepeats) {}
+
+    /**
+     * Sample client.
+     */
+    public static void main(String[] args) {}
+}
