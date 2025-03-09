866e05e30ae9bdb169708e9e8604216a0c330acd
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-09 09:42:22 -0400

    Update Bar data type

--- barchart/Bar.java
+++ barchart/Bar.java
@@ -1,23 +1,43 @@
-public class Bar implements Comparable<Bar> {
+/**
+ * A bar aggregates related information (name, value, and category) for use in
+ * a bar chart.
+ * 
+ * @author Yaohui Wu
+ */
+public final class Bar implements Comparable<Bar> {
+    private final String name;
+    private final int value;
+    private final String category;
+
     /**
      * Creates a new bar.
      */
-    public Bar(String name, int value, String category) {}
+    public Bar(String name, int value, String category) {
+        this.name = name;
+        this.value = value;
+        this.category = category;
+    }
 
     /**
      * Returns the name of this bar.
      */
-    public String getName() {}
+    public String getName() {
+        return name;
+    }
 
     /**
      * Returns the value of this bar.
      */
-    public int getValue() {}
+    public int getValue() {
+        return value;
+    }
 
     /**
      * Returns the category of this bar.
      */
-    public String getCategory() {}
+    public String getCategory() {
+        return category;
+    }
 
     /**
      * Compare two bars by value.
