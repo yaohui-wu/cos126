45ebfb7d45621b38e84a8556858f0e6c541f77fc
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-09 09:34:54 -0400

    Update bar

---
+++ barchart/Bar.java
@@ -0,0 +1,31 @@
+public class Bar implements Comparable<Bar> {
+    /**
+     * Creates a new bar.
+     */
+    public Bar(String name, int value, String category) {}
+
+    /**
+     * Returns the name of this bar.
+     */
+    public String getName() {}
+
+    /**
+     * Returns the value of this bar.
+     */
+    public int getValue() {}
+
+    /**
+     * Returns the category of this bar.
+     */
+    public String getCategory() {}
+
+    /**
+     * Compare two bars by value.
+     */
+    public int compareTo(Bar that) {}
+
+    /**
+     * Sample client.
+     */
+    public static void main(String[] args) {}
+}
